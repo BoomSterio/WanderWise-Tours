ca41a1537171c58d1cca3ced2063a9f9347d9089
Maksym <max.sharinov@gmail.com>
2023-10-29 03:06:07 +0300

    feat: added JWT token generation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,17 @@
+const jwt = require('jsonwebtoken')
+
 const User = require('../models/User')
 const catchAsync = require('../utils/catch-async')
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const user = await User.create(req.body)
+  const { name, email, password, passwordConfirm } = req.body
+  const user = await User.create({ name, email, password, passwordConfirm })
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN })
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user,
     },
