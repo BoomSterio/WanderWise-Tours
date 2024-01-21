d4503c4bb7c343b8499d49313dcf016e628562d5
Maksym <max.sharinov@gmail.com>
2024-01-21 22:56:29 +0200

    feat: improved tests for getRecursiveRoles

--- constants/user.spec.js
+++ constants/user.spec.js
@@ -1,15 +1,35 @@
 const { USER_ROLES, getRecursiveRoles } = require('./user')
 
+const { ADMIN, TECHNICIAN, LEAD_GUIDE, GUIDE, USER } = USER_ROLES
+
 describe(
   '🔎 Teting getRecursiveRoles function ' +
     'which accepts minimal required role ' +
-    'and returns an array containing THIS role and all roles that inherit it',
+    'and returns an array containing THIS role and all superior roles',
   () => {
     test('user role must return all roles', () => {
-      expect(getRecursiveRoles(USER_ROLES.USER).sort()).toStrictEqual(Object.values(USER_ROLES).sort())
+      const expected = Object.values(USER_ROLES).sort()
+      expect(getRecursiveRoles(USER).sort()).toStrictEqual(expected)
     })
+
     test('guide role must return guide and all roles of higher rank', () => {
-      expect(getRecursiveRoles(USER_ROLES.USER).sort()).toStrictEqual(Object.values(USER_ROLES).sort())
+      const expected = [ADMIN, TECHNICIAN, LEAD_GUIDE, GUIDE].sort()
+      expect(getRecursiveRoles(GUIDE).sort()).toStrictEqual(expected)
+    })
+
+    test('lead-guide role must return lead-guide and all roles of higher rank', () => {
+      const expected = [ADMIN, TECHNICIAN, LEAD_GUIDE].sort()
+      expect(getRecursiveRoles(LEAD_GUIDE).sort()).toStrictEqual(expected)
+    })
+
+    test('technician role must return technician and all roles of higher rank', () => {
+      const expected = [ADMIN, TECHNICIAN].sort()
+      expect(getRecursiveRoles(TECHNICIAN).sort()).toStrictEqual(expected)
+    })
+
+    test('admin role must return and all roles of higher rank', () => {
+      const expected = [ADMIN].sort()
+      expect(getRecursiveRoles(ADMIN).sort()).toStrictEqual(expected)
     })
   },
 )
