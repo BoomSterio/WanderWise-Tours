d8a40b68254309abdd8980c9815651243143e774
Maksym <max.sharinov@gmail.com>
2024-02-22 18:52:57 +0200

    feat: rendering real tours on overview page

--- controllers/view.js
+++ controllers/view.js
@@ -1,8 +1,14 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tour')
+const catchAsync = require('../utils/catch-async')
+
+exports.getOverview = catchAsync(async (req, res) => {
+  const tours = await Tour.find()
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   })
-}
+})
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
